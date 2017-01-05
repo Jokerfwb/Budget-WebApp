28a4085c14258ce6bf29b1590b1cea1246bdb9f9
Jokerfwb <jokerfwb@yahoo.com>
2017-01-05 18:28:32 -0500

    Week range

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,30 +5,49 @@ function Bill (data) {
 }
 
 var ViewModel = function() {
-	var self = this;
-  self.bill = {
-  	name : ko.observable(),
-    dueDate: ko.observable(),
-    dayDue: ko.observable(" "),
-    amount: ko.observable()
-	};
-  self.Bills = ko.observableArray([]);
-  ;
+    var self = this;
+
+    self.weekRange = ko.observableArray([]);
+    self.payFrequency = ko.observable();
+    self.payDay = ko.observable();
+    self.bill = {
+  	    name : ko.observable(" "),
+        dueDate: ko.observable("2017-01-01"),        
+        amount: ko.observable(" ")
+    };
+
+    self.Bills = ko.observableArray([]);
   
-  self.addBill = function() {
-  	self.Bills.push(new Bill(self.bill));
-    self.bill.name()
-    self.bill.dueDate()
-    self.bill.amount();
-    console.log(self.Bills());
-  }
   
-  self.getDay = function() {
-    var date = self.bill.dueDate();
-    date = moment(date).format('dddd');  
-    console.log(date);    
-    self.bill.dayDue(date);
-  }
+    self.addBill = function () {    
+        self.Bills.push(new Bill(self.bill));
+        self.resetForm();
+        console.log(self.Bills());
+    }
+
+    self.getWeekRange = function () {        
+        var a = moment(self.payDay()).format('LL');
+        self.weekRange.push(a);
+        for (var i = 0; i < 48; i++) {
+            a = moment(a).add(7, 'days').format('LL');
+            self.weekRange().push(a);
+        }
+        console.log(self.weekRange());
+    }
+
+
+    self.getDay = function() {
+        var date = self.bill.dueDate();
+        date = moment(date).format('dddd');  
+        console.log(date);    
+        self.bill.dayDue(date);
+    }
+
+    self.resetForm = function () {
+        self.bill.name(" ")
+        self.bill.dueDate("2017-01-01")
+        self.bill.amount(" ");
+    }
 }
 
 ko.applyBindings(new ViewModel());
